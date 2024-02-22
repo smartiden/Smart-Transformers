c1a0dc84698aa4300da4157e859d1221d9d42f3a
Arthur Zucker <arthur.zucker@gmail.com>
2024-02-22 22:15:51 +0900

    use very small images

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -142,7 +142,7 @@ class CircleCIJob:
         test_command = ""
         if self.command_timeout:
             test_command = f"timeout {self.command_timeout} "
-        test_command += f"python3 -m pytest --junitxml=test-results/junit.xml -n {self.pytest_num_workers} " + " ".join(pytest_flags)
+        test_command += f"python3 -m pytest --no-header --junitxml=test-results/junit.xml -n {self.pytest_num_workers} " + " ".join(pytest_flags)
 
         if self.parallelism == 1:
             if self.tests_to_run is None:
