94c05ebf9db92455bc0537ecd95a0e11039b18db
Arthur Zucker <arthur.zucker@gmail.com>
2024-02-23 15:39:04 +0900

    add test

--- .circleci/create_circleci_config.py
+++ .circleci/create_circleci_config.py
@@ -535,7 +535,7 @@ def create_circleci_config(folder=None):
     else:
         test_list = []
     if len(test_list) > 0:
-        jobs.extend([torch_job])
+        jobs.extend([torch_job, custom_tokenizers_job])
 
     #     extended_tests_to_run = set(test_list.split())
     #     # Extend the test files for cross test jobs
